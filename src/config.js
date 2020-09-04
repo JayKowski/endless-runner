6f4d2e02dbb643bc377168d19f25f3c1f8048807
JayKowski <jaymaina5@gmail.org>
2020-09-04 04:40:05 +0300

    changed some physics parameters

--- src/config.js
+++ src/config.js
@@ -1,20 +1,21 @@
 import Intro from "./phaserIntro";
 import MainGame from "./gameScene";
+import TestGame from './testGame';
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
-  height: 550,
+  height: 450,
   physics: {
     default: "arcade",
     arcade: {
-      debug: true,
-      gravity: { y: 200 },
+      // debug: true,
+      // gravity: { y: 200 },
       enableBody: true
     },
   },
-  scene: [ MainGame, Intro ],
+  scene: [ TestGame, MainGame, Intro ],
 };
 
 export default config;

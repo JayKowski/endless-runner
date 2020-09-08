583dcac41d6c4c9153f7f4fdfd41295be99d8968
JayKowski <jaymaina5@gmail.org>
2020-09-09 00:38:10 +0300

    delete condole.log statements

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,80 +1,46 @@
-// import "regenerator-runtime";
+const fetch = require('node-fetch');
 
-const fetch = require("node-fetch");
-const data = { "name" : "Bunny Hop" };
+const gameID = 'vG2Zfi8JOWxCujClMJ2Z';
 
-async function postGameApi() {
-    // creates the game in the API and get the ID 
-  const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games",{
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data),
-  });
-
-  const result = response.json();
-
-  result.then(res => console.log(res));
-}
+// creates the game in the API and get the ID
 
 async function getApi() {
-    let serverRes;
-    // gets the scores of specific game with the given ID
-    const response = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Z3N1DiIVq4C11F2FgrTg/scores",
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
     {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    });
-    const result = response.json();
-    result.then((res) => { 
-        // console.log(res)
-        serverRes = res;
-    }).catch((err) => console.log("OOPS! --> " + err));
-    return result;
-};
-
-async function scorePostApi(user, score) {
-    const data = {
-        user,
-        score,
-    }
-    console.log(data);
-  const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Z3N1DiIVq4C11F2FgrTg/scores",
-    {
-      method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = response.json();
-  result.then((res) => console.log(res));
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    });
+  return response.json();
 }
 
-// console.log(scorePostApi('Abunuwasi', 900));
-// console.log(getApi());
+async function scorePostApi(user, score) {
+  const data = {
+    user,
+    score,
+  };
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+    {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data),
+    },
+  );
+  return response.json();
+}
 
-// console.log(getApi())
-
-export { getApi, scorePostApi }
+export { getApi, scorePostApi };
